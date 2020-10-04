647c97f954e42613ecc2a362e5df55adb03b313b
HyungJun Goo <9h0jun1115@gmail.com>
2020-10-05 00:18:18 +0900

    ListView class based programming

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
-def all_rooms(request):
-    page = request.GET.get("page", 1) # 1 is default
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.get_page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+    """ Homeview Definition """
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+    
