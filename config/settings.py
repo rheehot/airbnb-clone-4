838ad7d2adcfd84c3fe5c56f8726f2e7ef762a72
HyungJun Goo <9h0jun1115@gmail.com>
2020-10-05 23:14:23 +0900

    Adapt CBV

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "@k18ozcq&km^u2*m_^==9@phn^t(5-ia1l$05frw=3^-v(h@#t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
